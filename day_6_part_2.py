7c9b9d159f9cc696b3707b252214df4442f8216b
Helen Bailey <hakbailey@gmail.com>
2019-12-09 22:33:19 -0500

    Day 6

---
+++ day_6_part_2.py
@@ -0,0 +1,29 @@
+with open("day_6_input.txt", "r") as f:
+    input = f.readlines()
+
+
+def get_indirect_orbits(object, indirect_orbits):
+    try:
+        new_object = orbits[object]['d']
+        indirect_orbits.append(new_object)
+        return get_indirect_orbits(new_object, indirect_orbits)
+    except KeyError:
+        return indirect_orbits
+
+
+orbits = {}
+
+for i in input:
+    orbiter = i.split(')')[1].rstrip('\n')
+    orbitee = i.split(')')[0]
+    if orbiter not in orbits:
+        orbits[orbiter] = {'d': orbitee}
+
+for o in orbits:
+    orbits[o]['i'] = get_indirect_orbits(orbits[o]['d'], [])
+
+
+y = set(orbits['YOU']['i'])
+s = set(orbits['SAN']['i'])
+
+print(len(y.symmetric_difference(s)) + 2)
